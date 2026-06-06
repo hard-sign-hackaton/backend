e734de01951936fd609db6810b5e677c39189136
Арсений Федоровцев <arsenui2006@mail.ru>
2026-06-06 21:40:24 +0500

    Починили мердж

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.config import settings
-from src.api import displays, health
+from src.api import displays, health, ws
+from src.logging_config import setup_logging
 
+setup_logging()
 app = FastAPI(title="Display API", version="1.0")
 
 app.add_middleware(
@@ -14,3 +16,4 @@ app.add_middleware(
 
 app.include_router(health.router)
 app.include_router(displays.router)
+app.include_router(ws.router)
