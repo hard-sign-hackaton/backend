924feff5708c6a09abad3c68304cf084a2d0b072
Sergei <seregasidorov2015@mail.ru>
2026-06-06 13:41:31 +0500

    Драйвер бд поменян на асинхронный

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "DisplayAPP"
     HOST: str = "0.0.0.0"
     PORT: int = 8000
-    DATABASE_URL: str = "postgresql+psycopg2://postgres:postgres@postgres-db:5432/DisplayDB"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@postgres-db:5432/KPOStudyTrackerDB"
     CORS_ORIGINS: List[str] = ["http://localhost:3000"]
     PASSWORD_SALT: str = "salt"
 
