821c2bb7d38514e25806bd90f4c05c46f64453bd
Sergei <seregasidorov2015@mail.ru>
2026-06-06 17:25:23 +0500

    В настройки добавлены поля для апи, прописан initial класс обращения к апи

--- src/config.py
+++ src/config.py
@@ -8,6 +8,9 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@postgres-db:5432/DisplayDB"
     CORS_ORIGINS: List[str] = ["http://localhost:3000"]
     PASSWORD_SALT: str = "salt"
+    API_KEY : str = "api_key"
+    API_URL: str = "https://hck-api.unicorn.icu"
+    MAX_RETRIES: int = 3
 
     class Config:
         env_file = ".env"
