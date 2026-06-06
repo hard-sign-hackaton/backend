db0baa40851333d9a7f64da72df07c79c6d8e99a
Sergei <seregasidorov2015@mail.ru>
2026-06-06 18:13:17 +0500

    Изменены запросы к апи на асинхронные

--- src/core/client_api.py
+++ src/core/client_api.py
@@ -1,5 +1,6 @@
 import httpx
 from ..config import settings
+from fastapi import HTTPException
 
 class ClientAPI:
     def __init__(self):
@@ -8,13 +9,15 @@ class ClientAPI:
         self.params = {"token": self.api_key}
                        
                        
-    def fetch_data(self, endpoint, **params):
+    async def fetch_data(self, endpoint, **params):
         params = {**self.params, **params}
-        try:
-            response = httpx.get(f"{self.url}/{endpoint}", params=params)
-            response.raise_for_status()
-            return response.json()
-        except httpx.HTTPError as e:
-            return {"error": str(e)}
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(f"{self.url}/{endpoint.lstrip('/')}", params=params)
+                response.raise_for_status()
+                if response['error'] == "1": raise HTTPException(404)
+                return response.json()
+            except httpx.HTTPError as e:
+                return {"error": str(e)}
     
     
