bee2474b50406af3e65c466f4d7dbbe11799a6a3
Sergei <seregasidorov2015@mail.ru>
2026-06-07 08:43:21 +0500

    Корс на 5173

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     HOST: str = "0.0.0.0"
     PORT: int = 8000
     DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@postgres-db:5432/DisplayDB"
-    CORS_ORIGINS: List[str] = ["http://localhost:3000"]
+    CORS_ORIGINS: List[str] = ["http://localhost:5173"]
     PASSWORD_SALT: str = "salt"
     API_KEY : str = "api_key"
     API_URL: str = "https://hck-api.unicorn.icu"
